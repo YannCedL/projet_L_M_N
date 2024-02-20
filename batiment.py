44b9c8023385b6cc3f327c1b3b5e81e31582a226
YannCedL <104587740+YannCedL@users.noreply.github.com>
2024-02-20 01:01:15 +0100

    Add class

---
+++ batiment.py
@@ -0,0 +1,9 @@
+from .infrastructure import *
+class Batiment:
+  def __init__(self,reseau):
+    self.id_bat=reseau.id_batiment
+    self.nb_mais=reseau.nb_maisons
+    self.maisons=[infra]
+  
+  
+
---
+++ infrastructure.py
@@ -0,0 +1,5 @@
+class infra:
+  def __init__(self,reseau):
+    self.id_inf=reseau.infra_id
+    self.type=reseau.infra_type
+    self.long=reseau.longueur

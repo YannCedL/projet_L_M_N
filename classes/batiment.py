e7b0e412e1fbcf950a9f7e6fb7a9ab535629825e
YannCedL <104587740+YannCedL@users.noreply.github.com>
2024-02-23 22:49:39 +0100

    Solution v1

--- classes/batiment.py
+++ classes/batiment.py
@@ -3,8 +3,8 @@ class Batiment:
   def __init__(self,id_batiment):
     self.id_bat=id_batiment
   
-  def diff_batiment(reseau):
-    bats=reseau.groupby('id_batiment')
+  def diff_batiment(df):
+    bats=df.groupby('id_batiment')
     return bats["diff_infra"].transform('sum')
   
 

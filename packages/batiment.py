cd1276e05800da0df8bc307032a3a4884cf8c500
Sandrine Nokam <sandrinewato27@gmail.com>
2024-02-29 22:57:39 +0100

    adding function add_infra which allws to add a list of infrastructure belongs to a building

--- packages/batiment.py
+++ packages/batiment.py
@@ -5,8 +5,9 @@ class Batiment:
     def __init__(self, id_building, list_infras) -> None:
         self.id_building = id_building
         self.building_diff = 0
-        if type(list_infras) == Infra: 
-            self.list_infras = list_infras
+        
+        self.list_infras = list_infras
+        
 
     #Cette méthode permet de calculer la difficulté d'un batiment en sommant les difficultés de ces infra
     def get_building_difficulty(self) -> float:
